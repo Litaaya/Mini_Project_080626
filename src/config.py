f0150dc48cab7610c797bd93bbae043fdd33a2d0
Litaaya <minhquans2810@gmail.com>
2026-08-07 12:04:19 +0700

    feat: add pipeline orche

---
+++ src/config.py
@@ -0,0 +1,22 @@
+import os
+from pathlib import Path
+
+base_dir = Path(__file__).resolve().parent.parent
+
+data_dir = base_dir / "data"
+raw_data_path = data_dir / "raw" / "data.csv"
+cleaned_data_path = data_dir / "processed" / "cleaned_data.csv"
+dlq_data_path = data_dir / "dlq"
+
+postgres_user = os.getenv("POSTGRES_USER", "postgres")
+postgres_password = os.getenv("POSTGRES_PASSWORD", "postgres")
+postgres_host = os.getenv("POSTGRES_HOST", "localhost")
+postgres_port = os.getenv("POSTGRES_PORT", "5432")
+postgres_database = os.getenv("POSTGRES_DATABASE", "mini_project_db")
+
+database_url = (
+    f"postgresql://{postgres_user}:{postgres_password}@"
+    f"{postgres_host}:{postgres_port}/{postgres_database}"
+)
+
+default_table_name = "cleaned_jobs"
---
+++ src/pipeline.py
@@ -0,0 +1,38 @@
+import logging
+from extract import extract
+from load import load
+from transform import transform
+from config import default_table_name
+
+logging.basicConfig(
+    level=logging.INFO,
+    format="%(asctime)s - [%(levelname)s] - %(message)s",
+    datefmt="%Y-%m-%d %H:%M:%S",
+)
+
+
+def run_pipeline() -> None:
+    logging.info("Pipeline started")
+
+    try:
+        logging.info("Extracting data")
+        df_raw = extract()
+        logging.info(f"extracted {len(df_raw)} records")
+
+        logging.info("Transforming data")
+        df_clean = transform(df_raw)
+        logging.info("Transforming data complete")
+
+        logging.info("Loading data")
+        load(df_clean, table_name=default_table_name)
+        logging.info("Loading data complete")
+
+        logging.info("Pipeline completed")
+
+    except Exception as e:
+        logging.error(f"Pipeline failed: {e}")
+        raise e
+
+
+if __name__ == "__main__":
+    run_pipeline()
