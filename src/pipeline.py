f0f0d7dde5b7909ab6bbe349722fc96b789418a2
Litaaya <minhquans2810@gmail.com>
2026-08-07 14:23:48 +0700

    feat: add validate record and clean/dlq path for pipeline

--- src/pipeline.py
+++ src/pipeline.py
@@ -1,8 +1,8 @@
 import logging
 from extract import extract
 from load import load
-from transform import transform
-from config import default_table_name, raw_data_path
+from transform import transform, validate_records
+from config import default_table_name, raw_data_path, cleaned_data_path, dlq_data_path
 
 logging.basicConfig(
     level=logging.INFO,
@@ -19,9 +19,14 @@ def run_pipeline() -> None:
         df_raw = extract(raw_data_path)
         logging.info(f"extracted {len(df_raw)} records")
 
+        df_valid, df_dlq = validate_records(df_raw)
+
         logging.info("Transforming data")
-        df_clean = transform(df_raw)
+        df_clean = transform(df_valid)
         logging.info("Transforming data complete")
+
+        df_clean.to_csv(cleaned_data_path, index=False)
+        df_dlq.to_csv(dlq_data_path, index=False)
 
         logging.info("Loading data")
         load(df_clean, table_name=default_table_name)
